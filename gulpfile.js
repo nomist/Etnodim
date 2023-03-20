5d3cd271028fb8f67284c18a830a0fd1bfc62413
Taisiia Simon <taisiya.simon@gmail.com>
2023-03-20 19:17:22 +0100

    gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,35 @@
-const { src, dest } = require('gulp');
+const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
 
-function styleTask() {
-    return src('src/style/*.scss')
+gulp.task('html', () => {
+    return gulp.src('src/*.html')
+        .pipe(gulp.dest('dest/'));
+});
+
+gulp.task('img', () => {
+    return gulp.src('src/img/*.png')
+        .pipe(gulp.dest('dest/img/'));
+});
+
+gulp.task('style', () => {
+    return gulp.src('src/style/*.scss')
         .pipe(sass.sync().on('error', sass.logError))
-        .pipe(dest('dest/style/'));
-}
+        .pipe(gulp.dest('dest/style/'));
+});
 
-exports.style = styleTask
+gulp.task('watch', () => {
+    gulp.watch('src/style/*.scss', gulp.series('style'));
+    gulp.watch('src/*.html', gulp.series('html'));
+});
+
+gulp.task('serve', () => {
+    browserSync.init({
+        server: './dest'
+    });
+    gulp.watch("dest/**/*.*").on('change', browserSync.reload);
+});
+
+gulp.task('default', gulp.parallel('style', 'html', 'img'));
+
+gulp.task('dev', gulp.series('default', gulp.parallel('watch', 'serve')));
